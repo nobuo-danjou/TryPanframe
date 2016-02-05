aeaed5b74bdef5badbad36ee230bbddabf753395
Nobuo Danjou <danjou@soffritto.org>
2016-02-05 10:06:07 +0900

    Initial Commit

---
+++ TryPanframe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryPanframe
+//
+//  Created by Nobuo Danjou on 2016/02/05.
+//  Copyright © 2016年 Nobuo Danjou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
